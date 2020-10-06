c7f0840b441e1281446dbd654232e410b259551f
Gabriel Raposo <gabriel.robertor@gmail.com>
2020-10-05 21:45:13 -0300

    Small changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ export function Hero() {
       <div className='realative z-30 flex flex-col items-center justify-center text-white'>
         <h1 className='teste text-6xl font-semibold text-center'>
           Welcome to <br />
-          <span className='teste hover:text-primary-500 font-bold text-transparent transition-all duration-200 ease-in-out border-t-4 border-b-4 cursor-pointer'>
+          <span className='teste font-bold text-transparent border-t-4 border-b-4 cursor-pointer'>
             AKIO
           </span>
         </h1>
-        <button className='hover:bg-white hover:text-primary-500 hover:broder-primary-500 px-4 py-2 mt-10 text-xl font-semibold text-white transition-all duration-500 ease-in-out bg-transparent border border-white rounded-lg'>
+        <button className='hover:bg-white hover:text-transparent hover:broder-primary-500 px-4 py-2 mt-10 text-xl font-semibold text-white transition-all duration-500 ease-in-out bg-transparent border border-white rounded-lg'>
           Look At Our Menu
         </button>
       </div>
